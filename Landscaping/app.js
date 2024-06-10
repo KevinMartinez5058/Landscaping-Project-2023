286112598bd0fabfab113f0b61df371df2877757
Kevin Martinez <157753600+KevinMartinez5058@users.noreply.github.com>
2024-06-10 01:36:35 -0500

    Add files via upload

---
+++ Landscaping/app.js
@@ -0,0 +1,12 @@
+
+"use strict";
+//scrolling anchor points
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document
+      .querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+
